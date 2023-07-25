fedc8d71d5c5b2b68b448d098116e01eaafbc40b
yahyaoztemir <yahya.oztemir66@gmail.com>
2023-07-25 22:48:01 +0300

    BetweenTwoSets

---
+++ Hackerrank/Easy/BetweenTwoSets/Solution.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+        int sonuc = 0 ; 
+        for(int i = a.get(a.size()-1) ; i <= b.get(0) ; i++){
+            boolean sart = true ; 
+            for(int sayi : a){
+                if(i % sayi != 0 )
+                    sart = false ; 
+            }
+            for(int sayi2 : b){
+                if(sayi2 % i != 0)
+                    sart = false ; 
+            }
+            if(sart)
+                sonuc++;
+        }
+        return sonuc; 
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        String[] brrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> brr = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            int brrItem = Integer.parseInt(brrTemp[i]);
+            brr.add(brrItem);
+        }
+
+        int total = Result.getTotalX(arr, brr);
+
+        bufferedWriter.write(String.valueOf(total));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
